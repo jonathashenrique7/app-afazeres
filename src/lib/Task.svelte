3020d4e5957c0f9eb59b6d66dadac3dbcf3fd623
jonathashenrique7 <grinncode@gmail.com>
2024-03-16 21:03:23 -0300

    Svelte.js -> Arquitetura inicial

---
+++ src/lib/Task.svelte
@@ -0,0 +1,5 @@
+<div class="border border-neutral-300 shadow-sm p-3 rounded-md space-x-2">
+    <input type="checkbox" class="cursor-pointer accent-blue-600" />
+
+    <span>Essa é minha tarefa!</span>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Task from "$lib/Task.svelte";
+    import "../app.css";
+</script>
+  
+<slot />
+
+<div class="p-4 h-screen flex justify-center items-center">
+    <Task />
+</div>
